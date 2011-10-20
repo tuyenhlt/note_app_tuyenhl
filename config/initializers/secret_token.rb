6037f0347fbcc1933fdc6ceecd1cc55250f6f83e
LongTuyen <longtuyen05cntt1@gmail.com>
2011-10-20 11:27:10 +0700

    frist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoteApp::Application.config.secret_token = '96c6e5e3b47317b253568056bbd26e41e856974346375f02bf2a95df2a7221d7b0fe0bba92873cd39941ec33c46b5f77d36ee41052ca9ccf456bd4106ab9401c'
